93f73fee688dbf3b92ab77e3b7de4b92a8dc1c26
JoseLuisPucChan <josechan211@gmail.com>
2017-02-15 18:54:10 -0600

    Creación de clases
    Se creo la Clase Triangulo
    Se creo la Clase TestTriangulo

---
+++ AreaDeUnTriangulo2/app/src/androidTest/java/Clases/TestTriangulo.java
@@ -0,0 +1,19 @@
+package Clases;
+
+/**
+ * Created by Luis Puc on 15/02/2017.
+ */
+
+public class TestTriangulo extends Triangulo{
+
+    private float funcionS ;
+    private  float areaTriangulo;
+
+    public void CalcularFuncionS() {
+
+    }
+
+    public  void CalcularArea (){
+
+    }
+}
---
+++ AreaDeUnTriangulo2/app/src/androidTest/java/Clases/Triangulo.java
@@ -0,0 +1,36 @@
+package Clases;
+
+/**
+ * Created by Luis Puc on 15/02/2017.
+ */
+
+public class Triangulo {
+    private float ladoA; // Declaracion del atributo del primer lado del triangulo
+    private float ladoB; // Declaracion del atributo del segundo lado del triangulo
+    private float ladoC; //Declaracion del atributo del tercer lado del triangulo
+
+    public void setLadoA (float A){
+        this.ladoA = A;
+    }
+
+    public float getLadoA(){
+        return ladoA;
+    }
+
+    public void setLadoB (float B){
+        this.ladoA = B;
+    }
+
+    public float getLadoB(){
+        return ladoB;
+    }
+    public void setLadoC (float C){
+        this.ladoA = C;
+    }
+
+    public float getLadoC(){
+        return ladoC;
+    }
+
+
+}
