85787ba2b5506a138879452d3c010f31052d8a57
Yairvega <vegayair@hotmail.com>
2017-02-15 19:26:19 -0600

    Seguimiento de Condificación de las clases
    -Agregación de comentarios
    -Codificación de metodos en la clase testTriangulo

--- AreaDeUnTriangulo2/app/src/androidTest/java/Clases/TestTriangulo.java
+++ AreaDeUnTriangulo2/app/src/androidTest/java/Clases/TestTriangulo.java
@@ -6,14 +6,24 @@ package Clases;
 
 public class TestTriangulo extends Triangulo{
 
-    private float funcionS ;
-    private  float areaTriangulo;
+    private float funcionS ;//Declaracionn del atributo funcionS del triangulo
+    private  float areaTriangulo;// Declaracion del atributo area del Triangulo
 
+    // Metodo para calcular la funcionS
     public void CalcularFuncionS() {
 
-    }
+        float ladoA = getLadoA(); // Obtención del lado A del triángulo
+        float ladoB = getLadoB(); // Obtención del lado B del triángulo
+        float ladoC = getLadoC(); // Obtención del lado C del triángulo
+        this.funcionS = (ladoA + ladoB + ladoC)/2; // Calculo de la funcionS
 
+    }//Fin del metodo para calcular la funcionS
+
+    //Metodo para calcular el area del triangulo
     public  void CalcularArea (){
-
-    }
+        float ladoA = getLadoA(); // Obtención del lado A del triángulo
+        float ladoB = getLadoB(); // Obtención del lado B del triángulo
+        float ladoC = getLadoC(); // Obtención del lado C del triángulo
+        this.areaTriangulo = (float) Math.sqrt(funcionS*(funcionS-ladoA)*(funcionS-ladoB)*(funcionS-ladoC)); //Calculo del area del triangulo en funcion a la variable funcionS
+    }//fin del metodo para calcular el area del triangulo
 }
--- AreaDeUnTriangulo2/app/src/androidTest/java/Clases/Triangulo.java
+++ AreaDeUnTriangulo2/app/src/androidTest/java/Clases/Triangulo.java
@@ -9,28 +9,33 @@ public class Triangulo {
     private float ladoB; // Declaracion del atributo del segundo lado del triangulo
     private float ladoC; //Declaracion del atributo del tercer lado del triangulo
 
+    // Asignacion de valor al lado a por medio de un parametro
     public void setLadoA (float A){
         this.ladoA = A;
-    }
+    }//Fin del metodo setLadoA
 
+    // Obtener los datos de la variable lado a
     public float getLadoA(){
         return ladoA;
-    }
+    }//Fin del metodo getLadoA
 
+    // Asignacion de valor al lado b por medio de un parametro
     public void setLadoB (float B){
         this.ladoA = B;
-    }
+    }//Fin del metodo setLadoB
 
+    // Obtener los datos de la variable lado b
     public float getLadoB(){
         return ladoB;
-    }
+    }//Fin del metodo getLadoB
+
+    // Asignacion de valor al lado c por medio de un parametro
     public void setLadoC (float C){
         this.ladoA = C;
-    }
+    }//Fin del metodo setLadoC
 
+    // Obtener los datos de la variable lado c
     public float getLadoC(){
         return ladoC;
-    }
-
-
+    }//Fin del metodo getLadoC
 }
